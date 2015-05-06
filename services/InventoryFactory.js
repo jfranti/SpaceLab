ef665d967dec52df8e2b3f0533043a85ac34688c
John Franti & Brian Kropff & Reid Baldwin & Patrick Fiorentino & James Nielson <student@epicodus.com>
2015-05-06 14:58:44 -0700

    Update Inventory Factory.

--- services/InventoryFactory.js
+++ services/InventoryFactory.js
@@ -1,6 +1,5 @@
-inventory.factory('InventoryFactory', function InventoryFactory() {
+spacelab.factory('InventoryFactory', function InventoryFactory() {
     var factory = {};
-
     //LIST OF POSSIBLE INVENTORY ITEMS
     factory.keycard = 0;
     factory.murder_weapon = 0;
@@ -10,41 +9,33 @@ inventory.factory('InventoryFactory', function InventoryFactory() {
     factory.soap = 0;
     factory.pda = 0;
     factory.dog_tags = 0;
-
+    //END OF INVENTORY LIST
     factory.pickup_keycard = function () {
         factory.keycard = 1;
     };
-
     factory.pickup_murder_weapon = function () {
         factory.murder_weapon = 1;
     };
-
     factory.pickup_lockbox = function () {
         factory.lockbox = 1;
     };
-
     factory.pickup_phaser = function () {
         factory.phaser = 1;
     };
-
     factory.pickup_towel = function () {
         factory.towel = 1;
     };
-
     factory.pickup_soap = function () {
         factory.soap = 1;
     };
-
     factory.pickup_pda = function () {
         factory.pda = 1;
     };
-
     factory.pickup_dog_tags = function () {
         factory.dog_tags = 1;
     };
-
-
-
-
-
-})
+    factory.drop_keycard = function () {
+        factory.keycard = 0;
+    };
+    return factory;
+});
