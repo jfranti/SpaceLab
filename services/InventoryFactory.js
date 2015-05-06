66f1256d644bebefda84acb407bde73273547eae
John Franti & Brian Kropff & Reid Baldwin & Patrick Fiorentino & James Nielson <student@epicodus.com>
2015-05-06 14:14:50 -0700

    add basic inv. fact.

---
+++ services/InventoryFactory.js
@@ -0,0 +1,50 @@
+inventory.factory('InventoryFactory', function InventoryFactory() {
+    var factory = {};
+
+    //LIST OF POSSIBLE INVENTORY ITEMS
+    factory.keycard = 0;
+    factory.murder_weapon = 0;
+    factory.lockbox = 0;
+    factory.phaser = 1;
+    factory.towel = 0;
+    factory.soap = 0;
+    factory.pda = 0;
+    factory.dog_tags = 0;
+
+    factory.pickup_keycard = function () {
+        factory.keycard = 1;
+    };
+
+    factory.pickup_murder_weapon = function () {
+        factory.murder_weapon = 1;
+    };
+
+    factory.pickup_lockbox = function () {
+        factory.lockbox = 1;
+    };
+
+    factory.pickup_phaser = function () {
+        factory.phaser = 1;
+    };
+
+    factory.pickup_towel = function () {
+        factory.towel = 1;
+    };
+
+    factory.pickup_soap = function () {
+        factory.soap = 1;
+    };
+
+    factory.pickup_pda = function () {
+        factory.pda = 1;
+    };
+
+    factory.pickup_dog_tags = function () {
+        factory.dog_tags = 1;
+    };
+
+
+
+
+
+})
