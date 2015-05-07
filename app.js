3884526804c0160735bdf6cf9d8c6fdb0088f107
James Nielson <james@changemachine.org>
2015-05-07 15:41:21 -0700

    fix audio loop issue? (jQuery script triggers autoplay)

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var spacelab = angular.module('spacelab', ['ui.router']);
 
 spacelab.config(function($stateProvider) {
-
   $stateProvider.state('start', {
     url: '',
     views: {
@@ -24,3 +23,10 @@ spacelab.config(function($stateProvider) {
     }
   });
 });
+
+
+var soundtrack = $("<audio id='soundtrack' src='audio/FoxSynergy_Blue_Space_08.mp3' autobuffer='auto' preload controls></audio>");
+$("#soundtrack").append(soundtrack);
+soundtrack.attr('autoplay','autoplay');
+
+$('#soundtrack')[0].play();
