522d73879200fa48deba52776d37e9a99fe611e2
James Nielson <james@changemachine.org>
2015-05-08 08:25:41 -0700

    Repairs to CSS mess. Structural adjustments in partials.

--- controllers/PlotController.js
+++ controllers/PlotController.js
@@ -5,42 +5,42 @@ spacelab.controller('PlotCtrl', function PlotCtrl($scope, PlotFactory) {
 
 
 
-  //MAIN RM METHODS/FUNCTIONS
-  $scope.lights_off_main = function () {
-    ng-show="/../IMG/Main_Room_Lights_Off.png";
-  }
-
-  $scope.main_rm = function () {
-    if ($scope.lights_main = 1) {
-      ng-hide="/../IMG/Main_Room_Lights_Off.png";
-      ng-show="/../IMG/Main_Room_no_knife.png";
-      ng-show="/../IMG/knife_body.png";
-      ng-hide="/../IMG/body_view.png";
-    }
-  }
-
-  $scope.view_body = function () {
-    if ($scope.pavels_body = 1) {
-      ng-hide="/../IMG/Main_Room_no_knife.png";
-      ng-hide="/../IMG/knife_body.png";
-      ng-show="/../IMG/body_view.png";
-    }
-  }
-
-  $scope.enter_pavels_rm = function () {
-    if ($scope.door = 1) {
-      ng-hide="/../IMG/Main_Room_no_knife.png";
-      ng-hide="/../IMG/knife_body.png";
-      ng-show="/../IMG/CrewQuarters.png";
-      ng-hide="/../IMG/mirrorface_female.png";
-
-    }
-  }
-
-  $scope.look_at_mirror = function () {
-    if ($scope.mirror = 1) {
-      ng-hide="/../IMG/CrewQuarters.png";
-      ng-show="/../IMG/mirrorface_female.png";
-    }
-  }
+//   //MAIN RM METHODS/FUNCTIONS
+//   $scope.lights_off_main = function () {
+//     ng-show="/../IMG/Main_Room_Lights_Off.png";
+//   }
+//
+//   $scope.main_rm = function () {
+//     if ($scope.lights_main = 1) {
+//       ng-hide="/../IMG/Main_Room_Lights_Off.png";
+//       ng-show="/../IMG/Main_Room_no_knife.png";
+//       ng-show="/../IMG/knife_body.png";
+//       ng-hide="/../IMG/body_view.png";
+//     }
+//   }
+//
+//   $scope.view_body = function () {
+//     if ($scope.pavels_body = 1) {
+//       ng-hide="/../IMG/Main_Room_no_knife.png";
+//       ng-hide="/../IMG/knife_body.png";
+//       ng-show="/../IMG/body_view.png";
+//     }
+//   }
+//
+//   $scope.enter_pavels_rm = function () {
+//     if ($scope.door = 1) {
+//       ng-hide="/../IMG/Main_Room_no_knife.png";
+//       ng-hide="/../IMG/knife_body.png";
+//       ng-show="/../IMG/CrewQuarters.png";
+//       ng-hide="/../IMG/mirrorface_female.png";
+//
+//     }
+//   }
+//
+//   $scope.look_at_mirror = function () {
+//     if ($scope.mirror = 1) {
+//       ng-hide="/../IMG/CrewQuarters.png";
+//       ng-show="/../IMG/mirrorface_female.png";
+//     }
+//   }
 });
